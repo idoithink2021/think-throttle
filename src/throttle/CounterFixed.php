9d3cfd90564e4c9e367edc7cf8f6f706a2e01dd6
idoithink2021 <76543270+idoithink2021@users.noreply.github.com>
2023-03-11 15:52:44 +0800

    Update CounterFixed.php

--- src/throttle/CounterFixed.php
+++ src/throttle/CounterFixed.php
@@ -3,8 +3,6 @@
 
 namespace think\middleware\throttle;
 
-use Psr\SimpleCache\CacheInterface;
-
 /**
  * 计数器固定窗口算法
  * Class CounterFixed
@@ -13,9 +11,9 @@ use Psr\SimpleCache\CacheInterface;
 class CounterFixed extends ThrottleAbstract
 {
 
-    public function allowRequest(string $key, float $micronow, int $max_requests, int $duration, CacheInterface $cache): bool
+    public function allowRequest(string $key, float $micronow, int $max_requests, int $duration, $cache)
     {
-        $cur_requests = (int) $cache->get($key, 0);
+        $cur_requests = $cache->get($key, 0);
         $now = (int) $micronow;
         $wait_reset_seconds = $duration - $now % $duration;     // 距离下次重置还有n秒时间
         $this->wait_seconds = $wait_reset_seconds % $duration  + 1;

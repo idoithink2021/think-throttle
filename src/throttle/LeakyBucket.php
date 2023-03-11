3cdb1a1b766bfe801656ca276ec4de36ac401ee9
idoithink2021 <76543270+idoithink2021@users.noreply.github.com>
2023-03-11 15:53:53 +0800

    Update LeakyBucket.php

--- src/throttle/LeakyBucket.php
+++ src/throttle/LeakyBucket.php
@@ -3,8 +3,6 @@
 
 namespace think\middleware\throttle;
 
-use Psr\SimpleCache\CacheInterface;
-
 /**
  * 漏桶算法
  * Class LeakyBucket
@@ -13,15 +11,15 @@ use Psr\SimpleCache\CacheInterface;
 class LeakyBucket extends ThrottleAbstract
 {
 
-    public function allowRequest(string $key, float $micronow, int $max_requests, int $duration, CacheInterface $cache): bool
+    public function allowRequest(string $key, float $micronow, int $max_requests, int $duration, $cache)
     {
         if ($max_requests <= 0) return false;
 
-        $last_time = (float) $cache->get($key, 0);      // 最近一次请求
+        $last_time = $cache->get($key, 0);      // 最近一次请求
         $rate = (float) $duration / $max_requests;       // 平均 n 秒一个请求
         if ($micronow - $last_time < $rate) {
             $this->cur_requests = 1;
-            $this->wait_seconds = ceil($rate - ($micronow - $last_time));
+            $this->wait_seconds = ceil($rate);
             return false;
         }
 
